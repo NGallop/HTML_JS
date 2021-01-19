78f360b2589a1d5264333571268130666db21df4
NGallop <62139335+NGallop@users.noreply.github.com>
2021-01-19 19:50:57 +0000

    Add files via upload

---
+++ JS/index.js
@@ -0,0 +1,7 @@
+let myName = 'Niall'; //string literal
+let age = 27; // number literal
+let isApproved = true; // boolean literal
+let firstName = undefined; // not set to anything
+let lastName = null; // nothing - something but nothing
+// others include object and symbol.
+console.log(myName);
